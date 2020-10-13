1e1a6104cbc49d93228bd2d79172185cca5c30a9
jeongabae <gabea52@gmail.com>
2020-10-13 22:45:21 +0900

    [update]Practice4_07

--- src/main/java/Practice4_07/BMImain.java
+++ src/main/java/Practice4_07/BMImain.java
@@ -13,14 +13,16 @@ public class BMImain {
         System.out.print("Enter: name1, age1, weight1(kg), height1(cm) : "); //User1 Information Input Guide
         BMI bmi1 = new BMI(scanner.next(), scanner.nextInt(),
                 scanner.nextDouble(), scanner.nextDouble()); //user1 information
+        double user1Bmi = bmi1.getBMI(); //user1 bmi
 
         System.out.print("Enter: name2, age2, weight2(kg), height2(cm) : "); //User2 Information Input Guide
         BMI bmi2 = new BMI(scanner.next(), scanner.nextInt(),
                 scanner.nextDouble(), scanner.nextDouble()); //user2 information
-
-        System.out.println("User1 BMI: "+bmi1.getBMI()); //Print user1's BMI index
-        System.out.println("User1 Interpretation: "+bmi1.getInterpretation(bmi1.getBMI()));    //Print user1's interpretation
-        System.out.println("User2 BMI: "+bmi2.getBMI());  //Print user2's BMI index
-        System.out.println("User2 Interpretation: "+bmi2.getInterpretation(bmi2.getBMI()));    //Print user2's interpretation
+        double user2Bmi = bmi2.getBMI(); //user2 bmi
+        System.out.println("User1 BMI: "+user1Bmi); //Print user1's BMI index
+        System.out.println("User1 Interpretation: "+bmi1.getInterpretation(user1Bmi));    //Print user1's interpretation
+        System.out.println("User2 BMI: "+user2Bmi);  //Print user2's BMI index
+        System.out.println("User2 Interpretation: "+bmi2.getInterpretation(user2Bmi));    //Print user2's interpretation
+        scanner.close();
     }
 }
